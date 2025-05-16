9f32e93e95c37611ba471300119f46ad1e6ab48a
malmstrm <malmstrm.stefan@gmail.com>
2025-05-16 13:42:30 +0200

    1.3.5 - ShapeParameter - Entity - Created

---
+++ Domain/Entities/ShapeParameter.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Commons;
+
+namespace Domain.Entities
+{
+    public class ShapeParameter : BaseEntity
+    {
+        public int ShapeCalculationId { get; set; }
+        public ParameterType ParameterType { get; set; }
+        public double Value { get; set; }
+
+        public ShapeCalculation ShapeCalculation { get; set; } = null!;
+    }
+}
