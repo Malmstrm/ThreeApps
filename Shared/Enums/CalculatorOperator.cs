8789ea33e3fb3a4c128b71cf68204da2d7c1da50
malmstrm <malmstrm.stefan@gmail.com>
2025-05-16 13:46:13 +0200

    1.3.8 - Enum CalculatorOperator Created in Enums

---
+++ Shared/Enums/CalculatorOperator.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared.Enums;
+
+public enum CalculatorOperator
+{
+    Addition,       // +
+    Subtraction,    // -
+    Multiplication, // *
+    Division,       // /
+    Modulus,        // %
+    SquareRoot      // √
+}
