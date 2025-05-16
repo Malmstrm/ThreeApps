9e5fbf7a7dcf2331e7da8d2e48e458bd91c4ca41
malmstrm <malmstrm.stefan@gmail.com>
2025-05-16 22:53:24 +0200

    1.7.7 NavigationService added to Shared service

---
+++ Shared/Services/NavigationService.cs
@@ -0,0 +1,39 @@
+﻿using Shared.Interfaces;
+using Spectre.Console;
+using System;
+
+
+namespace Shared.Services;
+
+public class NavigationService : INavigationService
+{
+    public string NavigateWithArrows(string title, params string[] options)
+    {
+        int index = 0;
+        ConsoleKey key;
+
+        do
+        {
+            Console.Clear();
+            AnsiConsole.MarkupLine($"[bold cyan]{title}[/]\n");
+
+            for (int i = 0; i < options.Length; i++)
+            {
+                if (i == index)
+                    AnsiConsole.MarkupLine($"[bold yellow]> {options[i]}[/]");
+                else
+                    AnsiConsole.MarkupLine($"  {options[i]}");
+            }
+
+            key = Console.ReadKey(true).Key;
+
+            if (key == ConsoleKey.UpArrow)
+                index = (index == 0) ? options.Length - 1 : index - 1;
+            else if (key == ConsoleKey.DownArrow)
+                index = (index + 1) % options.Length;
+
+        } while (key != ConsoleKey.Enter);
+
+        return options[index];
+    }
+}
