de6af5877da3c88bb7d146646702109263f2a147
malmstrm <malmstrm.stefan@gmail.com>
2025-05-16 22:47:32 +0200

    1.7.5 HostHelper added and IApp interface

---
+++ Shared/Helpers/HostHelper.cs
@@ -0,0 +1,26 @@
+﻿using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using Microsoft.Extensions.Hosting;
+using Shared.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Shared.Helpers;
+
+public static class HostHelper
+{
+    public static IApp BuildAndRun<TApp>() where TApp : class, IApp
+    {
+        var host = Host.CreateDefaultBuilder()
+            .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+            .ConfigureContainer<ContainerBuilder>(builder =>
+            {
+                // Registrera NavigationService + Spectre.Console helpers
+                builder.RegisterType<NavigationService>().As<INavigationService>().SingleInstance();
+                builder.RegisterType<TApp>().As<IApp>();
+            })
+            .Build();
+
+        using var scope = host.Services.CreateScope();
+        return scope.ServiceProvider.GetRequiredService<IApp>();
+    }
+}
