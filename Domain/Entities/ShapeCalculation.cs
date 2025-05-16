369d333b4f8e631f5837267e44457d45bc97e332
malmstrm <malmstrm.stefan@gmail.com>
2025-05-16 13:39:01 +0200

    1.3.3 - ShapeCalculation - Entity - Created

---
+++ Domain/Entities/ShapeCalculation.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Commons;
+
+namespace Domain.Entities
+{
+    public class ShapeCalculation : BaseEntity
+    {
+        public ShapeType ShapeType { get; set; }
+        public double Area { get; set; }
+        public double Perimeter { get; set; }
+        public ICollection<ShapeParameter> ShapeParameters { get; set; } = new List<ShapeParameter>();
+    }
+}
