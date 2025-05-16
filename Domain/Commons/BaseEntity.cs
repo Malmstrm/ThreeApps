62d609a99302c32ec752a9ae7cb0ca0401557950
malmstrm <malmstrm.stefan@gmail.com>
2025-05-16 13:27:35 +0200

    1.3.1 - BaseEntity - Entity - Created in Commons

---
+++ Domain/Commons/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Commons
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateOnly CreatedAt { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    }
+}
