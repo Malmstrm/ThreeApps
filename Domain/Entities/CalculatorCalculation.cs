486fa4f86f0669161da9f5b77f697fedec773400
malmstrm <malmstrm.stefan@gmail.com>
2025-05-16 13:36:39 +0200

    1.3.3 - CalculatorCalculation - Entity - Created

---
+++ Domain/Entities/CalculatorCalculation.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Commons;
+
+namespace Domain.Entities
+{
+    public class CalculatorCalculation : BaseEntity
+    {
+        public double FirstValue { get; set; }
+        public double? SecondValue { get; set; }
+        public CalculatorOperator Operator { get; set; }
+        public double Result { get; set; }
+    }
+}
