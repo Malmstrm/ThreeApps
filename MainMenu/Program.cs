cc8be6fe14d4df241781cf616873e3990e887624
malmstrm <malmstrm.stefan@gmail.com>
2025-05-17 22:52:20 +0200

    2.2 Change DI in program

--- MainMenu/Program.cs
+++ MainMenu/Program.cs
@@ -1,46 +1,30 @@
-﻿using Application.Interfaces;
-using Autofac;
+﻿using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Shared.Interfaces;
 using Shared.Services;
+
+using MainMenu;
 using Microsoft.Extensions.DependencyInjection;
 
 var host = Host.CreateDefaultBuilder(args)
     .UseServiceProviderFactory(new AutofacServiceProviderFactory())
     .ConfigureContainer<ContainerBuilder>(builder =>
     {
-        // Register Shared Services
         builder.RegisterType<NavigationService>().As<INavigationService>().SingleInstance();
 
-        // Register Apps
         //builder.RegisterType<ShapeApplication>().Named<IApp>("shape");
         //builder.RegisterType<CalculatorApplication>().Named<IApp>("calculator");
         //builder.RegisterType<RpsApplication>().Named<IApp>("rps");
+
+        // Registrera MenuRunner i DI
+        builder.RegisterType<MenuRunner>().AsSelf();
     })
     .Build();
 
 using var scope = host.Services.CreateScope();
 var container = (ILifetimeScope)scope.ServiceProvider.GetService(typeof(ILifetimeScope));
 
-// Get Navigation Service
-var navigation = container.Resolve<INavigationService>();
-
-// Display Main Menu with Arrows
-var selectedApp = navigation.NavigateWithArrows(
-    "Välj vilken app du vill köra:",
-    "shape",
-    "calculator",
-    "rps"
-);
-
-// Run the selected app
-//if (container.IsRegisteredWithName<IApp>(selectedApp))
-//{
-//    var app = container.ResolveNamed<IApp>(selectedApp);
-//    app.Run();
-//}
-//else
-//{
-//    Console.WriteLine("Ogiltigt val, program avslutas.");
-//}
+// Hämta MenuRunner via DI och kör
+var menu = container.Resolve<MenuRunner>();
+menu.Run();
