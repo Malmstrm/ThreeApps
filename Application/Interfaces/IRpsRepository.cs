1706363c3d20219a33d0fef516c633406a8bac19
malmstrm <malmstrm.stefan@gmail.com>
2025-05-21 22:49:04 +0200

    3.3 Create IRpsRepository

---
+++ Application/Interfaces/IRpsRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using System.Linq.Expressions;
+
+namespace Application.Interfaces
+{
+    public interface IRpsRepository
+    {
+        Task AddAsync(RPSGame game, CancellationToken ct = default);
+        Task<IReadOnlyList<RPSGame>> GetAllAsync(CancellationToken ct = default);
+        Task<int> CountAsync(Expression<Func<RPSGame, bool>> predicate, CancellationToken ct = default);
+    }
+}
