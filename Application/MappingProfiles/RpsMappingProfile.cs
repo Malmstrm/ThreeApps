8dcf4ca31697ae140b5e17302f66d02bd41f5902
malmstrm <malmstrm.stefan@gmail.com>
2025-05-23 19:06:52 +0200

    3.9 AutoMapp profile RpsMappingProfile added.

---
+++ Application/MappingProfiles/RpsMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.MappingProfiles;
+
+public class RpsMappingProfile : Profile
+{
+    public RpsMappingProfile()
+    {
+        CreateMap<RPSGame, RpsGameDTO>()
+            .ForMember(d => d.PlayedAt, c => c.MapFrom(s => s.CreatedAt));
+    }
+}
