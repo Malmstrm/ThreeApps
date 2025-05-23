e0c3adf052d172f2d689727c74bd2fd78ba5f349
malmstrm <malmstrm.stefan@gmail.com>
2025-05-23 18:49:11 +0200

    3.6 added RpsGameDTO

---
+++ Application/DTOs/RpsGameDTO.cs
@@ -0,0 +1,11 @@
+﻿using Shared.Enums;
+
+
+namespace Application.DTOs;
+
+public record RpsGameDTO(
+    int Id,
+    RPSMove PlayerMove,
+    RPSMove ComputerMove,
+    GameOutcome Outcome,
+    DateTime PlayedAt);
