7c37851e3a2ec7a47476a1a5678b211787fc125d
malmstrm <malmstrm.stefan@gmail.com>
2025-05-23 18:46:29 +0200

    3.5 Create RpsRepository

---
+++ Infrastructure/Repositories/RpsRepository.cs
@@ -0,0 +1,33 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Infrastructure.Repositories
+{
+    public class RpsRepository : IRpsRepository
+    {
+        private readonly AppDbContext _dbContext;
+        public RpsRepository(AppDbContext dbContext) => _dbContext = dbContext;
+
+        public Task AddAsync(RPSGame game, CancellationToken ct = default)
+        {
+            _dbContext.RPSGames.Add(game);
+            return _dbContext.SaveChangesAsync(ct);
+        }
+
+        public Task<int> CountAsync(Expression<Func<RPSGame, bool>> predicate, CancellationToken ct = default) =>
+            _dbContext.RPSGames.CountAsync(predicate, ct);
+
+        public async Task<IReadOnlyList<RPSGame>> GetAllAsync(CancellationToken ct = default)
+        {
+            var list = await _dbContext.RPSGames
+                .AsNoTracking()
+                .OrderByDescending(g => g.CreatedAt)
+                .ToListAsync(ct);
+
+            return list;
+        }
+    }
+}
