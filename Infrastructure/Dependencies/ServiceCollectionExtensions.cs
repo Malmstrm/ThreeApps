7efe395225a7603d081884f88f4491103f95ad37
malmstrm <malmstrm.stefan@gmail.com>
2025-05-23 19:09:50 +0200

    3.10 DI Rps.

---
+++ Infrastructure/Dependencies/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Infrastructure.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Dependencies;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddRps(this IServiceCollection services)
+    {
+        services.AddScoped<IRpsRepository, RpsRepository>();
+        services.AddScoped<IRpsService, RpsService>();
+        return services;
+    }
+}
