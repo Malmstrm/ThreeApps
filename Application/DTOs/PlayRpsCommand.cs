2ca2ccccf415d3df4721947a1b04caba9aee5fb2
malmstrm <malmstrm.stefan@gmail.com>
2025-05-23 18:53:27 +0200

    3.7 Create IRpsService, added record PlayRpsCommand

---
+++ Application/DTOs/PlayRpsCommand.cs
@@ -0,0 +1,5 @@
+﻿using Shared.Enums;
+
+namespace Application.DTOs;
+
+public record PlayRpsCommand(RPSMove PlayerMove);
---
+++ Application/Interfaces/IRpsService.cs
@@ -0,0 +1,11 @@
+﻿using Application.DTOs;
+
+namespace Application.Interfaces
+{
+    public interface IRpsService
+    {
+        Task<RpsGameDTO> PlayAsync(PlayRpsCommand cmd, CancellationToken ct = default);
+        Task<IReadOnlyList<RpsGameDTO>> GetHistoryAsync(CancellationToken ct = default);
+        Task<double> GetWinRateAsync(CancellationToken ct = default);
+    }
+}
