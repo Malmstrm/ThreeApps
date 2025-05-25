c11559ad265481c9cad2754073ea0fab76288b30
malmstrm <malmstrm.stefan@gmail.com>
2025-05-25 19:27:32 +0200

    4.0.1 ICalculatorRepository added

---
+++ Application/Interfaces/ICalculatorRepository.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface ICalculatorRepository
+    {
+        Task AddAsync(CalculatorCalculation calc, CancellationToken ct = default);
+        Task<IReadOnlyList<CalculatorCalculation>> GetAllAsync(CancellationToken ct = default);
+    }
+}
