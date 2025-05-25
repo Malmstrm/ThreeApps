155b261d0250e1e160233eeafc73da0609b6e78d
malmstrm <malmstrm.stefan@gmail.com>
2025-05-25 19:31:18 +0200

    4.0.3 CalculationDTO

---
+++ Application/DTOs/CalculationDTO.cs
@@ -0,0 +1,13 @@
+﻿using Shared.Enums;
+
+namespace Application.DTOs;
+
+public class CalculationDTO
+{
+    public int Id { get; init; }
+    public double Operand1 { get; init; }
+    public double? Operand2 { get; init; }
+    public CalculatorOperator Operator { get; init; }
+    public double Result { get; init; }
+    public DateOnly Date { get; init; }
+}
