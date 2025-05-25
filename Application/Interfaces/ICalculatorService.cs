87b2ffe67565929b470f8b027aa47d38fad8682e
malmstrm <malmstrm.stefan@gmail.com>
2025-05-25 19:33:07 +0200

    4.0.4 ICalculatorService

---
+++ Application/Interfaces/ICalculatorService.cs
@@ -0,0 +1,9 @@
+﻿using Application.DTOs;
+
+namespace Application.Interfaces;
+
+public interface ICalculatorService
+{
+    Task<CalculationDTO> CalculationAsync(CalculateCommand cmd, CancellationToken ct = default);
+    Task<IReadOnlyList<CalculationDTO>> GetHistoryAsync(CancellationToken ct = default);
+}
