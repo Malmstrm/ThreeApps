30f9e37cadd89e1a085c43f48fc055a154123e3e
malmstrm <malmstrm.stefan@gmail.com>
2025-05-26 08:46:40 +0200

    4.0.7 CalculatorRepository added

---
+++ Infrastructure/Repositories/CalculatorRepository.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class CalculatorRepository : ICalculatorRepository
+{
+    private readonly AppDbContext _db;
+    public CalculatorRepository(AppDbContext db) => _db = db;
+
+    public Task AddAsync(CalculatorCalculation calc, CancellationToken ct = default)
+    {
+        _db.Calculations.Add( calc );
+        return _db.SaveChangesAsync(ct);
+    }
+    public async Task<IReadOnlyList<CalculatorCalculation>> GetAllAsync(CancellationToken ct = default)
+    {
+        var list = await _db.Calculations
+            .AsNoTracking()
+            .OrderByDescending(c => c.CreatedAt)
+            .ToListAsync(ct);
+        return list;
+    }
+
+
+}
