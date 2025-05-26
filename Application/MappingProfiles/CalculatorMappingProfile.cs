51a62c11dcf16fa7d6f0dc430b069e4e8b9f62ce
malmstrm <malmstrm.stefan@gmail.com>
2025-05-26 08:40:55 +0200

    4.0.6 CalculatorMappingProfile added

---
+++ Application/MappingProfiles/CalculatorMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.MappingProfiles;
+
+public class CalculatorMappingProfile : Profile
+{
+    public CalculatorMappingProfile() 
+    {
+        CreateMap<CalculatorCalculation, CalculationDTO>()
+            .ForMember(d => d.Date, opt => opt.MapFrom(s => s.CreatedAt)); // Om CreatedAt är DateOnly → DateOnly→DateOnly, annars DateTime→DateTime
+    }
+}
