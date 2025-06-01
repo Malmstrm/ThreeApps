03eae3d3cb87d398144685565c821924c1a0bf06
malmstrm <malmstrm.stefan@gmail.com>
2025-06-01 15:22:27 +0200

    4.0.1 ParameterDTO to DTOs added

---
+++ Application/DTOs/ParameterDTO.cs
@@ -0,0 +1,10 @@
+﻿using Shared.Enums;
+
+namespace Application.DTOs
+{
+    public class ParameterDTO
+    {
+        public ParameterType ParameterType { get; init; }
+        public double Value { get; init; }
+    }
+}
