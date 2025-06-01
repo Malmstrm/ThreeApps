c56605c0cd14a4159ad553bd3c68e375a433c9f3
malmstrm <malmstrm.stefan@gmail.com>
2025-06-01 16:39:22 +0200

    4.0.7 IShapeService to interfaces added

---
+++ Application/Interfaces/IShapeService.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs;
+
+
+namespace Application.Interfaces;
+
+public interface IShapeService
+{
+    Task<ShapeCalculationDTO> CreateAsync(CreateShapeCommand cmd, CancellationToken ct = default);
+    Task<IReadOnlyList<ShapeCalculationDTO>> GetAllAsync(CancellationToken ct = default);
+    Task<ShapeCalculationDTO?> GetByIdAsyc(int id, CancellationToken ct = default);
+    Task UpdateAsync(UpdateShapeCommand cmd, CancellationToken ct = default);
+    Task DeleteAsync(int id, CancellationToken ct = default);
+}
