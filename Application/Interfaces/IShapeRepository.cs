977f944095421b87e541fec2a42f927ae07b6183
malmstrm <malmstrm.stefan@gmail.com>
2025-06-01 16:11:44 +0200

    4.0.5 IShapeRepository interfaces added

---
+++ Application/Interfaces/IShapeRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces;
+
+public interface IShapeRepository
+{
+    Task<ShapeCalculation> AddAsync(ShapeCalculation shape, CancellationToken ct = default);
+
+    Task<IReadOnlyList<ShapeCalculation>> GetAllAsync(CancellationToken ct = default);
+
+    Task<ShapeCalculation?> GetByIdAsync(int id, CancellationToken ct = default);
+
+    Task UpdateAsync(ShapeCalculation shape, CancellationToken ct = default);
+
+    Task DeleteAsync(int id, CancellationToken ct = default);
+}
