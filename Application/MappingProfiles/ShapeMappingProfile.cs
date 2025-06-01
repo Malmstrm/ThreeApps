f5496255f6cfc1d720c846629c34e7b99bb913ff
malmstrm <malmstrm.stefan@gmail.com>
2025-06-01 20:03:23 +0200

    4.0.9 AutoMapper profiles created

---
+++ Application/MappingProfiles/ShapeMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using Application.DTOs;
+using AutoMapper;
+using Domain.Entities;
+
+namespace Application.MappingProfiles;
+
+public class ShapeMappingProfile : Profile
+{
+    public ShapeMappingProfile()
+    {
+        CreateMap<ShapeCalculation, ShapeCalculationDTO>()
+            .ForMember(dest => dest.Date,
+                opt => opt.MapFrom(src => src.CreatedAt))
+            .ForMember(dest => dest.Parameters,
+                opt => opt.MapFrom(src =>
+                    src.ShapeParameters.Select(sp => new ParameterDTO()
+                    {
+                        ParameterType = sp.ParameterType,
+                        Value = sp.Value,
+                    })
+                    .ToList()
+                )
+            );
+    }
+}
