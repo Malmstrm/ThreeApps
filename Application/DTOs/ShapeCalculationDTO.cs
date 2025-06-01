efea0620d1b5633d08bbc66337ceb31f6b39b88e
malmstrm <malmstrm.stefan@gmail.com>
2025-06-01 15:56:00 +0200

    4.0.4 ShapeCalculationDTO to DTOs added

---
+++ Application/DTOs/ShapeCalculationDTO.cs
@@ -0,0 +1,13 @@
+﻿using Shared.Enums;
+
+namespace Application.DTOs;
+
+public class ShapeCalculationDTO
+{
+    public int Id { get; init; }
+    public ShapeType ShapeType { get; init; }
+    public double Area { get; init; }
+    public double Perimeter { get; init; }
+    public DateOnly Date { get; init; }
+    public IReadOnlyList<ParameterDTO> Parameters { get; init; } = Array.Empty<ParameterDTO>();
+}
