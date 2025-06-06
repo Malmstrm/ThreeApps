99bf11378026a99f25f3b8a57b27035d8ed26b25
malmstrm <malmstrm.stefan@gmail.com>
2025-06-06 23:04:36 +0200

    7.0.1 added record UpdateCalculationCommand

---
+++ Application/DTOs/UpdateCalculationCommand.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Enums;
+
+namespace Application.DTOs;
+
+public record UpdateCalculationCommand(
+    int Id,
+    double Operand1,
+    double? Operand2,
+    CalculatorOperator Operator);
