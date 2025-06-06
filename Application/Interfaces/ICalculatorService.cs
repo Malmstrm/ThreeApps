dd511002a586ae7692276595f27ca7f7c20f3ce1
malmstrm <malmstrm.stefan@gmail.com>
2025-06-06 23:06:03 +0200

    7.0.3 more interfaces added

--- Application/Interfaces/ICalculatorService.cs
+++ Application/Interfaces/ICalculatorService.cs
@@ -6,4 +6,7 @@ public interface ICalculatorService
 {
     Task<CalculationDTO> CalculationAsync(CalculateCommand cmd, CancellationToken ct = default);
     Task<IReadOnlyList<CalculationDTO>> GetHistoryAsync(CancellationToken ct = default);
+    Task<CalculationDTO?> GetByIdAsync(int id, CancellationToken ct = default);
+    Task<CalculationDTO> UpdateAsync(UpdateCalculationCommand cmd, CancellationToken ct = default);
+    Task DeleteAsync(int id, CancellationToken ct = default);
 }
