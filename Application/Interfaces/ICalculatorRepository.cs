3bb1eb240fda60ff236eefa71373ec49be97ec4f
malmstrm <malmstrm.stefan@gmail.com>
2025-06-06 23:05:20 +0200

    7.0.2 Added new interfaces

--- Application/Interfaces/ICalculatorRepository.cs
+++ Application/Interfaces/ICalculatorRepository.cs
@@ -6,5 +6,8 @@ namespace Application.Interfaces
     {
         Task AddAsync(CalculatorCalculation calc, CancellationToken ct = default);
         Task<IReadOnlyList<CalculatorCalculation>> GetAllAsync(CancellationToken ct = default);
+        Task<CalculatorCalculation?> GetByIdAsync(int id, CancellationToken ct = default);
+        Task<CalculatorCalculation> UpdateAsync(CalculatorCalculation entity, CancellationToken ct = default);
+        Task DeleteAsync(int id, CancellationToken ct = default);
     }
 }
